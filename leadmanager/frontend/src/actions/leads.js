8ad2b9ec331c24976df68c1197a06b6dbc412aa0
PremePanther <ahmedali.96@hotmail.com>
2019-11-06 12:14:48 -0500

    alerts and error handling done

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,7 +1,7 @@
 // all http requests happen here
 import axios from "axios";
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERROR } from "./types";
 
 //get leads
 export const getLeads = () => dispatch => {
@@ -39,5 +39,14 @@ export const addLead = lead => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const errors = {
+        msg: err.response.data,
+        status: err.response.status
+      };
+      dispatch({
+        type: GET_ERROR,
+        payload: errors
+      });
+    });
 };
--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -21,7 +21,11 @@ export class Form extends Component {
     const { name, email, message } = this.state;
     const lead = { name, email, message };
     this.props.addLead(lead);
-    console.log(lead);
+    this.setState({
+      name: "",
+      email: "",
+      message: ""
+    });
   };
 
   render() {
